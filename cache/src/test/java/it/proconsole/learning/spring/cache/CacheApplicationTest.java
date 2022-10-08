6e822f3cdcc2d812f648fb7ed5ad2e899afe40bf
Carlo Bobba <carlo.bobba@outlook.it>
2022-10-08 10:00:27 +0200

    [CACHE] Create cache learning project

---
+++ cache/src/test/java/it/proconsole/learning/spring/cache/CacheApplicationTest.java
@@ -0,0 +1,13 @@
+package it.proconsole.learning.spring.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheApplicationTest {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
