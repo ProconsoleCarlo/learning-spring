68e3e7d5d2b1575a8595b4944901056710215bcc
Carlo Bobba <carlo.bobba@lastminute.com>
2023-02-03 12:09:42 +0100

    [futures] Usage example of CompletableFuture

---
+++ async-processing/src/main/java/it/proconsole/GenericException.java
@@ -0,0 +1,7 @@
+package it.proconsole;
+
+public class GenericException extends RuntimeException {
+    public GenericException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ async-processing/src/main/java/it/proconsole/model/AggregatedResult.java
@@ -0,0 +1,15 @@
+package it.proconsole.model;
+
+import it.proconsole.repository.FastRepository;
+import it.proconsole.repository.SlowRepository;
+import it.proconsole.repository.SlowestRepository;
+
+import java.time.Duration;
+
+public record AggregatedResult(
+        FastRepository.Result fastRepoResult,
+        SlowestRepository.Result slowestRepoResult,
+        SlowRepository.Result slowRepoResult,
+        Duration executionTime
+) {
+}
